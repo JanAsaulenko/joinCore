e4c7dbd5a2a5178b1760c66d3fe1efbc4fb7eb0f
John <asaulenkojan@gmail.com>
2019-02-23 16:45:25 +0200

    Create user auth by gmail and by email and password

--- src/app/client/auth/registration/registration.component.ts
+++ src/app/client/auth/registration/registration.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {AuthService} from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  @Output() redirectToAuth = new EventEmitter();
 
-  constructor() { }
+
+  private successMessage;
+  private errorMessage;
+  public formGroup = new FormGroup({
+    mail: new FormControl(''),
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    password: new FormControl(''),
+    confirm_password: new FormControl('')
+  });
+
+  constructor(private auth: AuthService) {
+  }
 
   ngOnInit() {
   }
 
+  redirectToEnter() {
+    this.redirectToAuth.emit('redirect');
+  }
+
+  registrateUser() {
+    this.auth.doUserRegister(this.formGroup.value).then((value) => {
+      console.log(value);
+      this.successMessage = 'User registrate have been successfull';
+      this.redirectToAuth.emit('success'); ///
+    }).catch((error) => {
+      console.log(error);
+      this.errorMessage = `User registration failed`;
+    });
+  }
 }
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -4,17 +4,19 @@ import {CommonModule} from '@angular/common';
 import {ClientRoutingModule} from './client-routing.module';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
-import {CabinetComponent} from './cabinet/cabinet.component';
 import {HomeComponent} from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     ClientRoutingModule
   ],
-  declarations: [MainComponent, HeaderComponent, FooterComponent, CabinetComponent, HomeComponent, AuthComponent, RegistrationComponent]
+  declarations: [MainComponent, HeaderComponent, FooterComponent, HomeComponent, AuthComponent, RegistrationComponent]
 })
 export class ClientModule {
 }
